547178c5d82bb410884208ecb9145436276da0f1
Helen <astrotrout@gmail.com>
2016-08-17 15:58:38 +0100

    go live

---
+++ assets/scripts/custom.js
@@ -0,0 +1,23 @@
+﻿var preventOrphans = function(element) {
+	//Would suggest this is used for title text mainly
+	$(element).each(function(){
+	    var string = $(this).html();
+	    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+	    $(this).html(string);
+	});
+}
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
